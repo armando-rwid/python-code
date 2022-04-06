c3c09e72f014be3f731209ca49ecdbd68ab466f7
sys-armando <manusiwapatrick@gmail.com>
2022-04-06 10:13:21 +0700

    demo project

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+Demo project pertama dengan python
+'''
+
+print("Hello Python")
+print("Leader : Patrick Armando")
+print("Staff 1 : Victoria Armando")
+print("Staff 2 : Gwyneth Armando")
+print("Staff 3 : Kenzie Armando")
